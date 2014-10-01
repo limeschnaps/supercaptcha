f6cc5fa0822e28f7a024d2b66f4c0bc437ad25e1
africa itcase <africa@faunris-work>
2014-10-01 10:38:08 +0600

    change author

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
         'django>=1.7',
         'pillow'
     ],
-    author='Viktor Kotseruba',
-    author_email='barbuzaster@gmail.com',
+    author='Efimov Alexey',
+    author_email='alexey.efimov.89@gmail.com',
     description='captchafield for django newforms',
     license='MIT',
     keywords='forms web django',
